558412961e27e33cd77990a8f1081c5a05e9bb01
SamkoH6 <sam.hronec6@gmail.com>
2024-05-24 13:48:39 +0200

    Change of backend provider

--- src/Utils.jsx
+++ src/Utils.jsx
@@ -1,7 +1,10 @@
 const fetchData = async () => {
   try {
+    // const response = await fetch(
+    //   "https://cryptoanalyticsbackend.onrender.com/"
+    // );
     const response = await fetch(
-      "https://cryptoanalyticsbackend.onrender.com/"
+      "https://crypto-analytics-backend-77c14449f33c.herokuapp.com/"
     );
     const responseData = await response.json();
     console.log("Received data:", responseData.data);
